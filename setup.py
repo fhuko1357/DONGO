b2734bf74a254de18b284cde34deab2558ec13d4
fhuko1357 <95587230+fhuko1357@users.noreply.github.com>
2024-11-20 12:05:02 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ actions = [
 
 # Wait for a few seconds to give time to focus on the target application
 time.sleep(10)
-password = "TheDisa1a"
+password = "NyolongTerus"
 timeout = "10"
     
 for x, y, duration in actions:
@@ -56,10 +56,8 @@ def save_echo_to_batch(file_path, echo_text):
         file.write(f'\necho {echo_text}')
 
 def save_command():
-    clipboard_text = pyperclip.paste()
-    password_echo = 'LiteManager Password : TheDisa1a'  
+    clipboard_text = pyperclip.paste() 
     save_echo_to_batch('show.bat', f'LiteManager ID: {clipboard_text}')
-    save_echo_to_batch('show.bat', password_echo)
 
 if __name__ == "__main__":
     save_command()
