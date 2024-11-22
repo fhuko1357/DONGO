45c32947fc8aa800efda969f7f5d833ec46d10d8
fhuko1357 <95587230+fhuko1357@users.noreply.github.com>
2024-11-22 15:51:49 +0700

    Update show.bat

--- show.bat
+++ show.bat
@@ -5,5 +5,3 @@ python -c "import pyautogui as pag; pag.click(155, 554, duration=2)"
 python -c "import pyautogui as pag; pag.click(637, 417, duration=2)"
 python -c "import pyautogui as pag; pag.click(588, 10, duration=2)"
 echo ..........................................................
-echo User name : runneradmin
-echo User Pass : Jancuklah
